7575ea736262329d60628c0227fad62d4459118b
Lyndon Panton <lyndonpanton@gmail.com>
2023-08-21 22:04:20 +0100

    Ball can now be passed between players

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -7,12 +7,12 @@ using UnityEngine.Rendering;
 public class Ball : MonoBehaviour
 {
     public Rigidbody2D rb2d;
-    public const int speed = 50;
+    public const int speed = 8;
     // Start is called before the first frame update
     void Start()
     {
-        float xDirection = Random.Range(0, Mathf.PI) * speed;
-        float yDirection = Random.Range(0, Mathf.PI) * speed;
+        float xDirection = Random.Range(0, Mathf.PI);
+        float yDirection = Random.Range(0, Mathf.PI);
 
         if (Random.Range(0, 2) == 0)
         {
@@ -25,10 +25,12 @@ public class Ball : MonoBehaviour
         }
 
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2(
-            xDirection,
-            yDirection
-        ).normalized;
+        //rb2d.AddForce(
+        //    new Vector2(xDirection, yDirection) * speed,
+        //    ForceMode2D.Impulse
+        //);
+
+        rb2d.velocity = Random.onUnitSphere * speed;
     }
 
     // Update is called once per frame
