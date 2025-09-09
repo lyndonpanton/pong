bbc74326971a20c8d3ddb5dcff47cac729d7b65d
Lyndon Panton <LyndonPanton@gmail.com>
2025-09-09 11:37:13 +0100

    Setup: ImGui

--- game/main.cpp
+++ game/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+
+#include "imgui.h"
+#include "imgui-SFML.h"
 #include "SFML/Graphics.hpp"
 
 int main(int argc, char* argv[])
 {
 	sf::RenderWindow render_window(sf::VideoMode(1080, 720), "Pong");
+
+	ImGui::SFML::Init(render_window);
+	ImGui::GetStyle().ScaleAllSizes(1.5f);
+	ImGui::GetIO().FontGlobalScale = 1.5f;
+	sf::Clock delta_clock;
 
 	while (render_window.isOpen())
 	{
@@ -11,13 +19,27 @@ int main(int argc, char* argv[])
 
 		while (render_window.pollEvent(event))
 		{
+			ImGui::SFML::ProcessEvent(event);
+
 			if (event.type == sf::Event::Closed) render_window.close();
 		}
 
+		ImGui::SFML::Update(render_window, delta_clock.restart());
+
+		ImGui::Begin("Settings");
+
+		ImGui::Text("Hello World");
+
+		ImGui::End();
+
+
 		render_window.clear(sf::Color(0, 0, 0));
 
+		ImGui::SFML::Render(render_window);
 		render_window.display();
 	}
+
+	ImGui::SFML::Shutdown(render_window);
 
 	return 0;
 }
