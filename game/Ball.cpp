f418af777fe4a0524453a0da178de496952ec1eb
Lyndon Panton <LyndonPanton@gmail.com>
2025-09-12 14:27:51 +0100

    Feature: Ball velocity is reset on new game

--- game/Ball.cpp
+++ game/Ball.cpp
@@ -1,11 +1,13 @@
 #include "Ball.h"
 
+/* General *******************************************************************/
 Ball::Ball()
 	: m_position(new int[2] { 0, 0 })
-	, m_initial_position(new int[2] { m_position[0], m_position[1]})
+	, m_position_initial(new int[2] { m_position[0], m_position[1]})
 	, m_radius(30)
 	, m_point_count(16)
 	, m_velocity(new float[2] { -4, 4 })
+	, m_velocity_initial(new float[2] { m_velocity[0], m_velocity[1] })
 	, m_colour(new int[3] { 255, 255, 255 })
 {
 
@@ -13,10 +15,11 @@ Ball::Ball()
 
 Ball::Ball(const Ball& ball)
 	: m_position(ball.get_position())
-	, m_initial_position(new int[2] { m_position[0], m_position[1]})
+	, m_position_initial(new int[2] { m_position[0], m_position[1]})
 	, m_radius(ball.get_radius())
 	, m_point_count(ball.get_point_count())
 	, m_velocity(ball.get_velocity())
+	, m_velocity_initial(ball.get_velocity_initial())
 	, m_colour(ball.get_colour())
 {
 
@@ -30,10 +33,11 @@ Ball::Ball(
 	int* colour
 )
 	: m_position(position)
-	, m_initial_position(new int[2] { m_position[0], m_position[1]})
+	, m_position_initial(new int[2] { m_position[0], m_position[1] })
 	, m_radius(radius)
 	, m_point_count(point_count)
 	, m_velocity(velocity)
+	, m_velocity_initial(new float[2] { m_velocity[0], m_velocity[1] })
 	, m_colour(colour)
 {
 
@@ -45,6 +49,7 @@ Ball::~Ball()
 		<< get_position()[1] << ")" << std::endl;
 }
 
+/* Getters *******************************************************************/
 int* Ball::get_colour() const
 {
 	return m_colour;
@@ -60,6 +65,11 @@ int* Ball::get_position() const
 	return m_position;
 }
 
+int* Ball::get_position_initial() const
+{
+	return m_position_initial;
+}
+
 int Ball::get_radius() const
 {
 	return m_radius;
@@ -70,11 +80,17 @@ float* Ball::get_velocity() const
 	return m_velocity;
 }
 
+float* Ball::get_velocity_initial() const
+{
+	return m_velocity_initial;
+}
+
 bool Ball::get_is_moving() const
 {
 	return m_is_moving;
 }
 
+/* Setters *******************************************************************/
 void Ball::set_colour(int* colour)
 {
 	m_colour = colour;
@@ -90,6 +106,11 @@ void Ball::set_position(int* position)
 	m_position = position;
 }
 
+void Ball::set_position_initial(int* position)
+{
+	m_position_initial = position;
+}
+
 void Ball::set_radius(int radius)
 {
 	m_radius = radius;
@@ -100,15 +121,21 @@ void Ball::set_velocity(float* velocity)
 	m_velocity = velocity;
 }
 
+void Ball::set_velocity_initial(float* velocity)
+{
+	m_velocity_initial = velocity;
+}
+
 void Ball::set_is_moving(bool is_moving)
 {
 	m_is_moving = is_moving;
 }
 
+/* Other *********************************************************************/
 void Ball::reset()
 {
-	//set_is_moving(false);
-	set_position(m_initial_position);
+	set_position(m_position_initial);
+	// change to calculate new random velocity instead
+	set_velocity(m_velocity_initial);
 
-	// Calculate new random velocity instead
 }
--- game/Ball.h
+++ game/Ball.h
@@ -12,9 +12,10 @@ public:
 	int* m_colour;
 	int m_point_count;
 	int* m_position;
-	int* m_initial_position;
+	int* m_position_initial;
 	int m_radius;
 	float* m_velocity;
+	float* m_velocity_initial;
 	bool m_is_moving = false;
 
 	Ball();
@@ -25,15 +26,19 @@ public:
 	int* get_colour() const;
 	int get_point_count() const;
 	int* get_position() const;
+	int* get_position_initial() const;
 	int get_radius() const;
 	float* get_velocity() const;
+	float* get_velocity_initial() const;
 	bool get_is_moving() const;
 
 	void set_colour(int*);
 	void set_point_count(int);
 	void set_position(int*);
+	void set_position_initial(int*);
 	void set_radius(int);
 	void set_velocity(float*);
+	void set_velocity_initial(float*);
 	void set_is_moving(bool);
 
 	void reset();
